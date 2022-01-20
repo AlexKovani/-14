a85a48f5e7154f2b6d72c27551b6b688f2da0ea2
canya <canya@DESKTOP-0RBMO97>
2022-01-20 17:17:37 +0300

    Добавьте файлы проекта.

---
+++ Задача 14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите название животного cat или dog: ");
+            string name = Console.ReadLine();
+            if (name == "dog" || name == "Dog" || name == "DOG")
+            {
+                Dog dog = new Dog(name);
+                dog.ShowInfo();
+            }
+            else
+            {
+                if (name == "cat" || name == "Cat" || name == "CAT")
+                {
+                    Cat cat = new Cat(name);
+                    cat.ShowInfo();
+                }
+
+                Console.WriteLine("Ошибка ввода!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
